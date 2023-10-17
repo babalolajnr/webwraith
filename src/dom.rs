f18ae06e105b442f2b361cc13a9ce9fc3045f5b9
Abdulqudduus Babalola <babalolajnr@gmail.com>
2023-10-17 20:21:54 +0100

    feat: add pretty print

--- src/dom.rs
+++ src/dom.rs
@@ -16,6 +16,8 @@ pub enum NodeType {
     Text(String),
     /// An element node containing element data.
     Element(ElementData),
+    // A comment node containing a string.
+    Comment(String),
 }
 
 /// Represents the data associated with an HTML element.
@@ -47,3 +49,38 @@ pub fn elem(name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
         }),
     }
 }
+
+/// Creates a new comment node with the given data.
+pub fn comment(data: String) -> Node {
+    Node {
+        children: Vec::new(),
+        node_type: NodeType::Comment(data),
+    }
+}
+
+impl Node {
+    /// Pretty-prints the DOM tree rooted at this node.
+    pub fn pretty_print(&self) {
+        self.pretty_print_helper(0);
+    }
+
+    /// Helper function for pretty-printing the DOM tree.
+    fn pretty_print_helper(&self, indent: usize) {
+        match &self.node_type {
+            NodeType::Text(text) => {
+                println!("{}{}", " ".repeat(indent), text);
+            }
+            NodeType::Element(elem_data) => {
+                println!("{}<{}>", " ".repeat(indent), elem_data.tag_name);
+                for child in &self.children {
+                    child.pretty_print_helper(indent + 2);
+                }
+                println!("{}<\\{}>", " ".repeat(indent), elem_data.tag_name);
+            }
+            NodeType::Comment(comment) => {
+                println!("{}<!-- {} -->", " ".repeat(indent), comment);
+            }
+        }
+    }
+}
+
